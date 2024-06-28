f472a18be3bc28d08982952e7dcc2db5a677779d
Shahriar hossain <120326976+ShahriarParib@users.noreply.github.com>
2024-06-28 23:39:00 +0600

    Time: 376 ms (33.97%), Space: 160.4 MB (25.17%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
         long long ans =0;
-        unordered_map<int,int>m;
+        unordered_map<int,long long>m;
         for(int i=0;i<n;i++)
         {
             m[i]=0;
@@ -20,13 +20,10 @@ public:
         sort(fala.begin(),fala.end());
         for(int i=0;i<fala.size();i++)
         {
-            m[fala[i].second]=i+1;
+            m[fala[i].second] *=(i+1);
+            ans +=m[fala[i].second];
         }
-          for(auto p:roads)
-        {
-            ans=ans+m[p[0]]+m[p[1]];
-
-        }
+        
         return ans;
     }
 };
